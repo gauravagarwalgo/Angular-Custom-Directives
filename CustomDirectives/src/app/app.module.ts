973d5c83256492bf94842a585f29380a54d5fab6
Gaurav Kumar6 <Gaurav_Kumar6@epam.com>
2023-05-24 10:32:11 +0530

    Custom directives code

---
+++ CustomDirectives/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { SetBackgroundDirective } from './CustomDirectives/setbackground.directive';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SetBackgroundDirective
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
