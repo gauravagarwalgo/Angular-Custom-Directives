f952a430cb427781260082fd8cf3706ab9f540d6
Gaurav Kumar6 <Gaurav_Kumar6@epam.com>
2023-05-27 14:08:02 +0530

    Binding Directive properties

--- CustomDirectives/src/app/CustomDirectives/better-highlight.directive.ts
+++ CustomDirectives/src/app/CustomDirectives/better-highlight.directive.ts
@@ -1,23 +1,32 @@
-import { Directive, ElementRef, HostBinding, HostListener, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, Renderer2, Input,OnInit } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
-export class BetterHighlightDirective {
+export class BetterHighlightDirective implements OnInit {
 
-  constructor(private element:ElementRef,private renderer:Renderer2) {
+  constructor(private element:ElementRef,private renderer:Renderer2)  {
 
    }
-   @HostBinding('style.backgroundColor') background:string='transparent';
+
+   @Input() defaultColor:string='transparent';
+   @Input() highlightColor:string='pink';
+   
+   @HostBinding('style.backgroundColor') background:string=this.defaultColor;
    @HostBinding('style.border') border:string='none';
+
+   //To make the color as yellow when the page first loads, otherwise it was coming as transparent
+   ngOnInit(){
+      this.background=this.defaultColor;
+   }
 
    @HostListener('mouseenter') mouseenter()
    {
-      this.background='pink';
+      this.background=this.highlightColor;
       this.border='red 2px solid'
    }
    @HostListener('mouseleave') mouseleave(){
-      this.background='transparent';
+      this.background=this.defaultColor;
       this.border='none'
    }
 }
